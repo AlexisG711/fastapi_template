e63a3bd91e4cdb6ec48f30d8c5240abe9b76ffbe
Alexis Gonzalez <alexis@tete-chercheuse.fr>
2024-07-24 14:46:43 +0200

    Fix test_main.py

--- test_main.py
+++ test_main.py
@@ -1,11 +1,12 @@
 import pytest
-from httpx import AsyncClient, ASGITransport
+from httpx import AsyncClient
+from asgi_lifespan import LifespanManager
 from main import app
 
 @pytest.mark.asyncio
 async def test_ping():
-    transport = ASGITransport(app)
-    async with AsyncClient(transport=transport, base_url="http://test") as ac:
-        response = await ac.get("/ping")
-    assert response.status_code == 200
-    assert response.json() == "pong"
+    async with LifespanManager(app):
+        async with AsyncClient(app=app, base_url="http://test") as ac:
+            response = await ac.get("/ping")
+        assert response.status_code == 200
+        assert response.json() == {"message": "pong"}
